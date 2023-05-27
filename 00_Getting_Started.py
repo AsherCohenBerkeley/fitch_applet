29e057a290d9be3ca027660679574441d4c16d17
Asher Cohen <ashercohen@berkeley.edu>
2023-05-26 19:42:19 -0700

    edits with getting started page

--- 00_Getting_Started.py
+++ 00_Getting_Started.py
@@ -6,9 +6,11 @@ st.title(':books: Getting Started')
 
 st.write("Hello, everyone! Welcome to Philos W12A's interface for writing and checking Fitch-style natural deduction proofs! If you're still feeling shaky about natural deduction even after watching the lectures, don't worry. The practice you get from this website should really help solidify a lot of the key concepts taught in the course.")
 
+st.write("To familiarize yourself with the basics of how to use this applet, we recommend you read the entirety of this 'Getting Started' page before you start writing your natural deduction proofs. Reading this page will also help review some key concepts from the course.")
+
 st.subheader("LaTeX")
 
-st.write("This website uses LaTeX as the required syntax for inputting formulas and rules into Fitch-style proofs. Hopefully, over the last couple weeks, you have gotten comfortable with the LaTeX syntax used to write common logical expressions. For convenience, we have a table shown below, reminding you of several important LaTeX commands.")
+st.write("This website uses LaTeX as the required syntax for inputting formulas and rules into Fitch-style proofs. Hopefully, over the last couple weeks, you have gotten comfortable with LaTeX in the context of typesetting common logical expressions. Nevertheless, for convenience, we have constructed the table shown below to help remind you of several important LaTeX commands.")
 
 st.write('')
 st.markdown(f"<center><b>Propositional Logic</b></center>", unsafe_allow_html=True)
@@ -61,10 +63,9 @@ st.write('')
 # st.write('')
 # st.write('')
 
-st.write('To type in a list of formulas (like when you want to input several premises for your proof), simply seperate each formula by a comma.')
+st.write('In this applet, whenever you want to type in a list of formulas (like when you want to input several premises for your proof), simply seperate each formula by a comma.')
 
 st.write("Additionally, so far in the course, we've only talked about using LaTeX for logical formulas. To use this website however, you'll also have to learn how to type out our various natural deduction rules in LaTeX. This works exactly as you might expect: simply replace each logical symbol by its corresponding LaTeX command.")
-
 
 st.write('')
 st.markdown(f"<center><b>Deduction Rule Examples</b></center>", unsafe_allow_html=True)
@@ -93,7 +94,7 @@ st.write('')
 
 st.subheader("Supported Deduction Rules")
 
-st.write("This website supports exactly those deduction rules taught in Philosophy W12A. Remember that this set of rules is complete with respect to the semantics taught in class, meaning you should never need more rules than what we've shown you to prove a given valid form of argument.")
+st.write("This website supports exactly those deduction rules taught in Philosophy W12A. Remember that this set of rules is complete with respect to the semantics taught in class, meaning you should never need more rules than what we've shown you to prove that a given form of argument is valid.")
 
 st.write("For convenience, here is a comprehensive list of all those rules taught in class where # stands in for some line number.")
 
@@ -120,14 +121,17 @@ st.write("**IMPORTANT NOTE**: All of the above rules can be used 'liberally' as 
 
 st.subheader("Writing and Checking Proofs")
 
-st.write("Otherwise, everything should work as described in lecture. Start with some premises, and then use a clever combination of opening subproofs and applying deduction rules as needed to hopefully arrive at your desired conclusion.")
+st.write("Otherwise, everything should work as described in lecture. Start with some premises, and then use a clever combination of opening subproofs and applying deduction rules to arrive at your desired conclusion.")
 
-st.write("But what's most useful about this applet is that it will allow you to **automatically check your proofs**! Recall from class that an important property of our proof system is that there exists an algorithm, which, in a finite amount of time, can decide whether a purported proof is in fact a correct syntactic proof. Well, this website has exactly that algorithm built into it! At any time (either while you're writing your proof or once you think you're finished), feel free to click the **'Check Proof' button**. If your proof is right, the proof checker will give you a big green check mark as congratulations (:white_check_mark:). And if your proof is wrong, the proof checker will tell you exactly what lines are faulty!")
+st.write("But the most useful feature of this applet is its **automatic proof checker**. Recall from class that an important property of our proof system is that there exists an algorithm, which, in a finite amount of time, can decide whether a purported proof is in fact a correct syntactic proof. Well, this website has exactly that algorithm built into it! At any time (either while you're writing your proof or once you think you've finished), feel free to click the **'Check Proof' button**. If your proof is right, the proof checker will give you a big green check mark as congratulations (:white_check_mark:). And if your proof is wrong, the proof checker will tell you exactly what lines are faulty!")
 
-st.write("**IMPORTANT NOTE**: Even if the proof checker only signals that a couple lines are faulty, that doesn't mean those are the only lines that need to be changed in order to arrive at a correct proof. Often, incorrect proofs may require substantial reorganization before they are syntactically correct.")
+st.write("**IMPORTANT NOTE**: Even if the proof checker only signals that a couple lines are faulty, that does not mean those are the only lines that need to be changed in order to arrive at a correct proof. Incorrect proofs often require substantial reorganization before they are syntactically correct.")
 
 st.subheader("Additional Help")
 
-st.write("If at any point, you're having trouble with any part of this website, please feel free to make a post on Ed Discussion. This website was also developed very recently, so it's possible there are some minor bugs in the underlying code. If you think you may have noticed a bug, again please make a post on Ed Discussion, detailing exactly what you did to arrive at the bug, ideally including screenshots.")
+st.write("If at any point you're having trouble with any part of this website, please feel free to make a post on Ed Discussion. We (the course staff) would be more than happy to help.")
 
+st.write("This website was also developed very recently, so it's possible there are some minor bugs in the underlying code. If you think you may have noticed a bug, again please make a post on Ed Discussion, detailing exactly what you did to arrive at the bug, ideally including screenshots.")
+
+st.write('')
 st.write("Thank you so much for reading along, and good luck with your Fitch-style proofs!")
